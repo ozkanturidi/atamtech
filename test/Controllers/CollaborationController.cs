3852642bc03d7ee5d0c188d62df75c4b256b4dbb
Yazganptr <82478281+Yazganptr@users.noreply.github.com>
2022-01-24 03:32:44 +0300

    Collaboration controller and model added

---
+++ test/Controllers/CollaborationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test.Models;
+
+namespace test.Controllers
+{
+    
+
+    public class CollaborationController : Controller
+    {
+
+        Context c = new Context();
+        
+        public ActionResult ColList()
+        {
+            var col = c.Collaborations.ToList();
+            return View(col);
+        }
+    }
+}
