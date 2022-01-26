f314e78d836159fc1c038a8a953e988dac10f9fd
Yazganptr <82478281+Yazganptr@users.noreply.github.com>
2022-01-26 16:42:05 +0300

    Projects Controller ve View eklendi

---
+++ test/Controllers/ProjectController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test.Models;
+
+
+namespace test.Controllers
+{
+    public class ProjectController : Controller
+    {
+        Context c = new Context();    
+
+        public ActionResult ProjectList()
+        {
+            var projects = c.Projects.ToList();
+           
+            return View(projects);
+        }
+
+        public ActionResult ProjectDetails(int id)
+        {
+
+            var projects = c.Projects.Where(x => x.id == id).ToList();
+            return View(projects);
+
+
+        }
+
+
+    }
+}
