a947563d095b17fad9baa2d611857f7705ed9a36
ikbalorhan <ikbalorhan@gmail.com>
2022-02-14 19:06:27 +0300

    hepsi ok

--- test/Controllers/HomeController.cs
+++ test/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using test.Models;
@@ -43,9 +45,36 @@ namespace test.Controllers
             c.Contacts.Add(cont);
             c.SaveChanges();
 
-
+            /*var name = Request["mail"];
+            Email(name);*/
+            
 
             return View();
         }
+
+
+      /*  public static void Email(string htmlString)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("FromMailAddress");
+                message.To.Add(new MailAddress("ToMailAddress"));
+                message.Subject = "Test";
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = htmlString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("FromMailAddress", "password");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception) { }
+        }
+
+    */
     }
 }
